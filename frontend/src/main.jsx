1b3f1460348e8453a3d7904fd08cf2c0fd19ca25
Munna Kumar Pandit <96653272+Munna764592@users.noreply.github.com>
2024-01-31 00:55:15 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,7 +6,7 @@ import { createTheme, ThemeProvider } from "@mui/material";
 import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./contexts/AuthContext.jsx";
 import axios from "axios";
-axios.defaults.baseURL = "https://aichatbot-qkt8.onrender.com";
+axios.defaults.baseURL = "https://aichatbot-qkt8.onrender.com/api/v1";
 axios.defaults.withCredentials = true;
 
 const theme = createTheme({
